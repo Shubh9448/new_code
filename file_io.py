a30e31fbaad74985305a3920a8a400878cc137b5
shubh9448 <shubh.patial@gmail.com>
2017-08-12 01:33:18 +0530

    second commit

--- file_io.py
+++ file_io.py
@@ -83,16 +83,27 @@
 # print(artist)
 # print(year)
 
-with open("sample1.txt", 'a') as tables:
-    for i in range(2, 13):
-        for j in range(1, 13):
-            print("{1:>2} times {0} is {2}".format(i, j, i*j), file=tables)
-        print("-" * 20, file=tables)
+# with open("sample1.txt", 'a') as tables:
+#     for i in range(2, 13):
+#         for j in range(1, 13):
+#             print("{1:>2} times {0} is {2}".format(i, j, i*j), file=tables)
+#         print("-" * 20, file=tables)
 
 
-
-
-
+# import shelve
+# books = shelve.open("book")
+# books["recipes"] = {"blt": ["bacon", "lettuce", "tomato", "bread"],
+#                      "beans_on_toast": ["beans", "bread"],
+#                      "scrambled eggs": ["eggs", "butter", "milk"],
+#                      "soup": ["tin of soup"],
+#                      "pasta": ["pasta", "cheese"]}
+# books["maintenance"] = {"stuck": ["oil"], "loose":["gaffer tape"]}
+#
+#
+# print(books["recipes"])
+# # print(books["recipes"]["scrambled eggs"])
+# #
+# # print(books["maintenance"]["loose"])
 
 
 
